6406404cc13c0fc38201271278e09a3876fad0ac
farmpro-iwate <sekiguchi_a@cameo.plala.or.jp>
2026-07-17 18:37:04 +0900

    Attach auth token to axios requests

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,25 @@
 import axios from 'axios';
 import { Cattle, CattleInput } from '../types/cattle';
+import { clearAuth, getAuthToken } from './authClient';
 
 export const api = axios.create({ baseURL: '/api' });
+
+api.interceptors.request.use((config) => {
+  const token = getAuthToken();
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 401) {
+      clearAuth();
+      if (window.location.pathname !== '/login') window.location.assign('/login');
+    }
+    return Promise.reject(error);
+  }
+);
 
 export async function getCattleList() { const res = await api.get<Cattle[]>('/cattle'); return res.data; }
 export async function getCattle(id: string) { const res = await api.get<Cattle>(`/cattle/${id}`); return res.data; }
