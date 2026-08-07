9ed2c83943e2ee9364396ca179134f14aa78302b
farmpro-iwate <sekiguchi_a@cameo.plala.or.jp>
2026-08-07 11:53:15 +0900

    Type stored calf lineage fields

--- client/src/types/calf.ts
+++ client/src/types/calf.ts
@@ -10,6 +10,9 @@ export type Calf = {
   birthday: string;
   sex: string;
   motherName: string;
+  geneticMotherCowName?: string;
+  recipientCowName?: string;
+  sireName?: string;
   startWeight: number;
   currentWeight: number;
   elapsedDays: number;
