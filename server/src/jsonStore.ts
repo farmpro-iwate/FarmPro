1c6a288a7ff71efe0aa09728afe668176ff1fc24
farmpro-iwate <sekiguchi_a@cameo.plala.or.jp>
2026-08-21 18:22:47 +0900

    Store bank transfer applications globally

--- server/src/jsonStore.ts
+++ server/src/jsonStore.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import { currentFarmId } from './farmContext';
 
-const GLOBAL_FILES = new Set(['users.json']);
+const GLOBAL_FILES = new Set(['users.json', 'bank-transfer-applications.json']);
 const DEFAULT_FARM_ID = 'farm-demo';
 
 function runtimeDataDir() {
