fa2089597bf0ad2fc8bf2eab8ec1467add49cd75
farmpro-iwate <sekiguchi_a@cameo.plala.or.jp>
2026-08-23 18:12:43 +0900

    Route synchronization AI and ET execution

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,6 +27,7 @@ import { BreedingForm } from './pages/BreedingForm';
 import { HeatRegistrationForm } from './pages/HeatRegistrationForm';
 import { BreedingExecutionForm } from './pages/BreedingExecutionForm';
 import { SynchronizationProgramForm } from './pages/SynchronizationProgramForm';
+import { SynchronizationBreedingExecutionForm } from './pages/SynchronizationBreedingExecutionForm';
 import { VaccineList } from './pages/VaccineList';
 import { VaccineForm } from './pages/VaccineForm';
 import { BlvList } from './pages/BlvList';
@@ -120,6 +121,7 @@ export default function App() {
 
       <Route path="/breedings" element={<RequireRegistration><AppLayout><BreedingList /></AppLayout></RequireRegistration>} />
       <Route path="/breedings/new" element={<RequireRegistration><AppLayout><HeatRegistrationForm /></AppLayout></RequireRegistration>} />
+      <Route path="/breedings/synchronization/:kind" element={<RequireRegistration><AppLayout><SynchronizationBreedingExecutionForm /></AppLayout></RequireRegistration>} />
       <Route path="/breedings/:id/insemination" element={<RequireRegistration><AppLayout><BreedingExecutionForm kind="insemination" /></AppLayout></RequireRegistration>} />
       <Route path="/breedings/:id/transfer" element={<RequireRegistration><AppLayout><BreedingExecutionForm kind="transfer" /></AppLayout></RequireRegistration>} />
       <Route path="/breedings/:id/edit" element={<RequireRegistration><AppLayout><BreedingForm mode="edit" /></AppLayout></RequireRegistration>} />
