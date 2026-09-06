2a2c98b94c74481b65f3c3c5a09c1439e7907d10
farmpro-iwate <sekiguchi_a@cameo.plala.or.jp>
2026-09-06 11:55:16 +0900

    Make home breeding entry choose AI or ET

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -170,7 +170,8 @@ export default function App() {
       <Route path="/breedings" element={<RequireRegistration><AppLayout><BreedingList /></AppLayout></RequireRegistration>} />
       <Route path="/breedings/new" element={<RequireRegistration><AppLayout><HeatRegistrationWithVideoHelp /></AppLayout></RequireRegistration>} />
       <Route path="/breedings/method" element={<RequireRegistration><AppLayout><BreedingMethodSelect /></AppLayout></RequireRegistration>} />
-      <Route path="/breedings/insemination/new" element={<RequireRegistration><AppLayout><InseminationRegistrationForm /></AppLayout></RequireRegistration>} />
+      <Route path="/breedings/insemination/new" element={<RequireRegistration><AppLayout><BreedingMethodSelect /></AppLayout></RequireRegistration>} />
+      <Route path="/breedings/ai/new" element={<RequireRegistration><AppLayout><InseminationRegistrationForm /></AppLayout></RequireRegistration>} />
       <Route path="/breedings/transfer-plan/new" element={<RequireRegistration><AppLayout><EmbryoTransferPlanForm /></AppLayout></RequireRegistration>} />
       <Route path="/breedings/synchronization/:kind" element={<RequireRegistration><AppLayout><SynchronizationBreedingExecutionForm /></AppLayout></RequireRegistration>} />
       <Route path="/breedings/:id/insemination" element={<RequireRegistration><AppLayout><BreedingExecutionForm kind="insemination" /></AppLayout></RequireRegistration>} />
